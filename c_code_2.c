5ce45461a4ff510a1e29a350a963c30fea4ddd65
Humphryshikunzi <humphryshikunzi9@gmail.com>
2021-02-16 07:34:35 +0300

    Emabaking on FirmWare

---
+++ c_code_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int age;
+    printf("Hello Dudes, am here again to write C Code\n");
+    printf("Enter your age\n");
+    scanf("%d",&age);
+    printf("Humphry Age is %d\n", age);
+    return 0;
+}
